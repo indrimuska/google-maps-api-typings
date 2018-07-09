53c0c5eabfe8e2c8e97eab1726e094f3bb99009e
Indri Muska <indrimuska@gmail.com>
2018-07-09 20:23:47 +0200

    Add `Directions` API

--- index.d.ts
+++ index.d.ts
@@ -169,11 +169,21 @@ declare module '@google/maps' {
         /**
          * The Directions API is a service that calculates directions between locations.
          * You can search for directions for several modes of transportation, including transit, driving, walking, or cycling.
+         * 
+         * @see https://developers.google.com/maps/documentation/directions/intro
          */
-        directions(query: DirectionsQuery, callback?: ResponseCallback<DirectionsResult>): RequestHandle<DirectionsResult>;
+        directions(query: DirectionsRequest, callback?: ResponseCallback<DirectionsResult>): RequestHandle<DirectionsResult>;
+        /**
+         * The Distance Matrix API is a service that provides travel distance and time for a matrix of origins and destinations.
+         * The API returns information based on the recommended route between start and end points, as calculated by the Google Maps API,
+         * and consists of rows containing duration and distance values for each pair.
+         * 
+         * @see https://developers.google.com/maps/documentation/distance-matrix/intro
+         */
+        distanceMatrix(query: DistanceMatrixRequest, callback?: ResponseCallback<DistanceMatrixResult>): RequestHandle<DistanceMatrixResult>;
     }
 
-    export interface DirectionsQuery {
+    export interface DirectionsRequest {
         /**
          * The address, textual latitude/longitude value, or place ID from which you wish to calculate directions.
          * 
@@ -193,9 +203,9 @@ declare module '@google/maps' {
         /**
          * Specifies the mode of transport to use when calculating directions
          * 
-         * @default DirectionsTravelMode.Driving
+         * @default TravelMode.Driving
          */
-        mode?: DirectionsTravelMode;
+        mode?: TravelMode;
         /**
          * Specifies an array of waypoints.
          * Waypoints alter a route by routing it through the specified location(s).
@@ -211,7 +221,7 @@ declare module '@google/maps' {
          */
         alternatives?: boolean;
         /** Indicates that the calculated route(s) should avoid the indicated features */
-        avoid?: Array<DirectionsAvoid>;
+        avoid?: Array<TravelRestriction>;
         /**
          * The language in which to return results.
          * 
@@ -257,9 +267,9 @@ declare module '@google/maps' {
          * It is possible the `best_guess` travel time prediction may be *shorter* than `optimistic`, or alternatively, *longer* than `pessimistic`,
          * due to the way the `best_guess` prediction model integrates live traffic information.
          * 
-         * @default DirectionsTrafficModel.BestGuess
+         * @default TrafficModel.BestGuess
          */
-        traffic_model?: DirectionsTrafficModel;
+        traffic_model?: TrafficModel;
         /**
          * Specifies one or more preferred modes of transit.
          * This parameter may only be specified for transit directions, and only if the request includes an API key or
@@ -284,7 +294,7 @@ declare module '@google/maps' {
      * **Note:** Both walking and bicycling directions may sometimes not include clear pedestrian or bicycling paths,
      * so these directions will return warnings in the returned result which you must display to the user.
      */
-    export enum DirectionsTravelMode {
+    export enum TravelMode {
         /** (default) indicates standard driving directions using the road network. */
         Driving = 'driving',
         /** requests walking directions via pedestrian paths & sidewalks (where available). */
@@ -300,7 +310,7 @@ declare module '@google/maps' {
         Transit = 'transit',
     }
 
-    export enum DirectionsAvoid {
+    export enum TravelRestriction {
         /** indicates that the calculated route should avoid toll roads/bridges. */
         tolls = 'tolls',
         /** indicates that the calculated route should avoid highways. */
@@ -327,7 +337,7 @@ declare module '@google/maps' {
 
     export interface DirectionsResult {
         /** contains metadata on the request */
-        status: DirectionsStatus;
+        status: DirectionsResultStatus;
         /** contains an array with details about the geocoding of origin, destination and waypoints */
         geocoded_waypoints: DirectionsGeocodedWaypoint[];
         /** contains an array of routes from the origin to the destination */
@@ -340,7 +350,7 @@ declare module '@google/maps' {
         available_travel_modes: string[];
     }
 
-    export enum DirectionsTrafficModel {
+    export enum TrafficModel {
         /**
          * (default) indicates that the returned `duration_in_traffic` should be the best estimate of travel time given what is known about
          * both historical traffic conditions and live traffic. Live traffic becomes more important the closer the departure_time is to now.
@@ -385,7 +395,7 @@ declare module '@google/maps' {
      * The `status` field within the Directions response object contains the status of the request, and may contain debugging information
      * to help you track down why the Directions service failed.
      */
-    export enum DirectionsStatus {
+    export enum DirectionsResultStatus {
         /** indicates the response contains a valid `result`. */
         OK = 'OK',
         /** indicates at least one of the locations specified in the request's origin, destination, or waypoints could not be geocoded. */
@@ -877,5 +887,223 @@ declare module '@google/maps' {
          * such as "Empire State Building" or "Statue of Liberty."
          */
         point_of_interest = 'point_of_interest',
+    }
+
+    export interface DistanceMatrixRequest {
+        /**
+         * The starting point for calculating travel distance and time.
+         * You can supply one or more locations separated by the pipe character (`|`), in the form of an address, latitude/longitude coordinates,
+         * or a place ID:
+         *  - If you pass an address, the service geocodes the string and converts it to a latitude/longitude coordinate to calculate distance.
+         *      This coordinate may be different from that returned by the Geocoding API, for example a building entrance rather than its center.
+         *      `origins=Bobcaygeon+ON|24+Sussex+Drive+Ottawa+ON`
+         *  - If you pass latitude/longitude coordinates, they are used unchanged to calculate distance.
+         *      Ensure that no space exists between the latitude and longitude values.
+         *      `origins=41.43206,-81.38992|-33.86748,151.20699`
+         *  - If you supply a place ID, you must prefix it with `place_id:`.
+         *      You can only specify a place ID if the request includes an API key or a Google Maps APIs Premium Plan client ID.
+         *      You can retrieve place IDs from the Geocoding API and the Places SDK (including Place Autocomplete).
+         *      `origins=place_id:ChIJ3S-JXmauEmsRUcIaWtf4MzE`
+         *  - Alternatively, you can supply an encoded set of coordinates using the Encoded Polyline Algorithm.
+         *      This is particularly useful if you have a large number of origin points, because the URL is significantly shorter when using
+         *      an encoded polyline.
+         *      - Encoded polylines must be prefixed with `enc:` and followed by a colon (`:`). For example: `origins=enc:gfo}EtohhU:`
+         *      - You can also include multiple encoded polylines, separated by the pipe character (`|`).
+         *          For example: `origins=enc:wc~oAwquwMdlTxiKtqLyiK:|enc:c~vnAamswMvlTor@tjGi}L:|enc:udymA{~bxM:`
+
+         */
+        origins: Array<LatLng>;
+        /**
+         * One or more locations to use as the finishing point for calculating travel distance and time.
+         * The options for the destinations parameter are the same as for the origins parameter, described above.
+         */
+        destinations: Array<LatLng>;
+        /**
+         * Specifies the mode of transport to use when calculating distance.
+         * Valid values and other request details are specified in the Travel Modes section of this document.
+         * 
+         * @default TravelMode.Driving
+         */
+        mode?: TravelMode;
+        /**
+         * The language in which to return results.
+         *  - If `language` is not supplied, the API attempts to use the preferred language as specified in the `Accept-Language` header,
+         *      or the native language of the domain from which the request is sent.
+         *  - The API does its best to provide a street address that is readable for both the user and locals. To achieve that goal,
+         *      it returns street addresses in the local language, transliterated to a script readable by the user if necessary,
+         *      observing the preferred language. All other addresses are returned in the preferred language.
+         *      Address components are all returned in the same language, which is chosen from the first component.
+         *  - If a name is not available in the preferred language, the API uses the closest match.
+         *  - The preferred language has a small influence on the set of results that the API chooses to return,
+         *      and the order in which they are returned. The geocoder interprets abbreviations differently depending on language,
+         *      such as the abbreviations for street types, or synonyms that may be valid in one language but not in another.
+         *      For example, utca and tér are synonyms for street in Hungarian.
+         */
+        language?: string;
+        /**
+         * The region code, specified as a [ccTLD](https://en.wikipedia.org/wiki/CcTLD) (country code top-level domain) two-character value.
+         * Most ccTLD codes are identical to ISO 3166-1 codes, with some exceptions.
+         * This parameter will only influence, not fully restrict, results from the geocoder.
+         * If more relevant results exist outside of the specified region, they may be included.
+         */
+        region?: string;
+        /**
+         * Introduces restrictions to the route. Valid values are specified in the Restrictions section of this document.
+         * Only one restriction can be specified.
+         */
+        avoid?: Array<TravelRestriction>;
+        /** Specifies the unit system to use when expressing distance as text */
+        units?: UnitSystem;
+        /**
+         * Specifies the desired time of arrival for transit requests, in seconds since midnight, January 1, 1970 UTC.
+         * You can specify either `departure_time` or `arrival_time`, but not both.
+         * Note that `arrival_time` must be specified as an integer.
+         */
+        arrival_time?: Date | number;
+        /**
+         * The desired time of departure. You can specify the time as an integer in seconds since midnight, January 1, 1970 UTC.
+         * Alternatively, you can specify a value of now, which sets the departure time to the current time (correct to the nearest second).
+         * 
+         * The departure time may be specified in two cases:
+         *  - For requests where the travel mode is transit: You can optionally specify one of `departure_time` or `arrival_time`.
+         *      If neither time is specified, the `departure_time` defaults to now (that is, the departure time defaults to the current time).
+         *  - For requests where the travel mode is driving: You can specify the `departure_time` to receive a route and trip duration
+         *      (response field: `duration_in_traffic`) that take traffic conditions into account.
+         *      This option is only available if the request contains a valid API key, or a valid
+         *      Google Maps APIs Premium Plan client ID and signature.
+         *      The `departure_time` must be set to the current time or some time in the future. It cannot be in the past.
+         * 
+         *      **Note:** Distance Matrix requests specifying `departure_time` when `mode=driving` are limited
+         *      to a maximum of 100 elements per request. The number of origins times the number of destinations defines the number of elements.
+         */
+        departure_time?: Date | number;
+        /**
+         * Specifies the assumptions to use when calculating time in traffic.
+         * This setting affects the value returned in the `duration_in_traffic` field in the response,
+         * which contains the predicted time in traffic based on historical averages.
+         * The `traffic_model` parameter may only be specified for requests where the travel mode is `driving`,
+         * and where the request includes a `departure_time`, and only if the request includes an API key or
+         * a Google Maps APIs Premium Plan client ID.
+         * 
+         * @default TrafficModel.BestGuess
+         */
+        traffic_model?: TrafficModel;
+        /** Specifies one or more preferred modes of transit. This parameter may only be specified for requests where the `mode` is `transit` */
+        transit_mode?: Array<TransitMode>;
+        /**
+         * Specifies preferences for transit requests. Using this parameter, you can bias the options returned,
+         * rather than accepting the default best route chosen by the API.
+         * This parameter may only be specified for requests where the `mode` is `transit`.
+         */
+        transit_routing_preference?: TransitRoutingPreference;
+    }
+
+    export interface DistanceMatrixResult {
+        /** contains metadata on the request. See Status Codes below. */
+        status: DistanceMatrixResultTopLevelStatus;
+        /**
+         * When the top-level status code is other than `OK`, this field contains more detailed information
+         * about the reasons behind the given status code.
+         */
+        error_message: string;
+        /**
+         * contains an array of addresses as returned by the API from your original request.
+         * These are formatted by the geocoder and localized according to the language parameter passed with the request.
+         */
+        origin_addresses: string;
+        /**
+         * contains an array of addresses as returned by the API from your original request.
+         * As with origin_addresses, these are localized if appropriate.
+         */
+        destination_addresses: string[];
+        /** contains an array of elements, which in turn each contain a status, duration, and distance element */
+        rows: DistanceMatrixRow[];
+    }
+
+    /** 
+     * The status fields within the response object contain the status of the request, and may contain useful debugging information.
+     * The Distance Matrix API returns a top-level status field, with information about the request in general,
+     * as well as a status field for each element field, with information about that particular origin-destination pairing.
+     */
+    export enum DistanceMatrixResultTopLevelStatus {
+        /** indicates the response contains a valid result */
+        OK = 'OK',
+        /** indicates that the provided request was invalid */
+        INVALID_REQUEST = 'INVALID_REQUEST',
+        /** indicates that the product of origins and destinations exceeds the per-query limit */
+        MAX_ELEMENTS_EXCEEDED = 'MAX_ELEMENTS_EXCEEDED',
+        /**
+         * indicates any of the following:
+         *  - The API key is missing or invalid.
+         *  - Billing has not been enabled on your account.
+         *  - A self-imposed usage cap has been exceeded.
+         *  - The provided method of payment is no longer valid (for example, a credit card has expired).
+         * See the [Maps FAQ](https://developers.google.com/maps/faq#over-limit-key-error) to learn how to fix this.
+         */
+        OVER_DAILY_LIMIT = 'OVER_DAILY_LIMIT',
+        /** indicates the service has received too many requests from your application within the allowed time period */
+        OVER_QUERY_LIMIT = 'OVER_QUERY_LIMIT',
+        /** indicates that the service denied use of the Distance Matrix service by your application */
+        REQUEST_DENIED = 'REQUEST_DENIED',
+        /** indicates a Distance Matrix request could not be processed due to a server error. The request may succeed if you try again */
+        UNKNOWN_ERROR = 'UNKNOWN_ERROR',
+    }
+
+    export enum DistanceMatrixResultElementLevelStatus {
+        /** indicates the response contains a valid result */
+        OK = 'OK',
+        /** indicates that the origin and/or destination of this pairing could not be geocoded */
+        NOT_FOUND = 'NOT_FOUND',
+        /** indicates no route could be found between the origin and destination */
+        ZERO_RESULTS = 'ZERO_RESULTS',
+        /** indicates the requested route is too long and cannot be processed */
+        MAX_ROUTE_LENGTH_EXCEEDED = 'MAX_ROUTE_LENGTH_EXCEEDED',
+    }
+
+    /**
+     * When the Distance Matrix API returns results, it places them within a JSON `rows` array.
+     * Even if no results are returned (such as when the origins and/or destinations don't exist), it still returns an empty array.
+     * XML responses consist of zero or more `<row>` elements.
+     * 
+     * Rows are ordered according to the values in the `origin` parameter of the request.
+     * Each row corresponds to an origin, and each `element` within that row corresponds to a pairing of the origin with a `destination` value.
+     * 
+     * Each `row` array contains one or more `element` entries, which in turn contain the information about a single origin-destination pairing.
+     */
+    export interface DistanceMatrixRow {
+        elements: DistanceMatrixRowElement[];
+    }
+
+    /** The information about each origin-destination pairing is returned in an `element` entry */
+    export interface DistanceMatrixRowElement {
+        /** possible status codes  */
+        status: DistanceMatrixResultElementLevelStatus;
+        /**
+         * The length of time it takes to travel this route, expressed in seconds (the `value` field) and as `text`.
+         * The textual representation is localized according to the query's `language` parameter.
+         */
+        duration: Duration;
+        /**
+         * The length of time it takes to travel this route, based on current and historical traffic conditions.
+         * See the `traffic_model` request parameter for the options you can use to request that the returned value is
+         * `optimistic`, `pessimistic`, or a `best-guess` estimate. The duration is expressed in seconds (the `value` field) and as `text`.
+         * The textual representation is localized according to the query's `language` parameter.
+         * The duration in traffic is returned only if all of the following are true:
+         *  - The request includes a `departure_time` parameter.
+         *  - The request includes a valid API key, or a valid Google Maps APIs Premium Plan client ID and signature.
+         *  - Traffic conditions are available for the requested route.
+         *  - The `mode` parameter is set to `driving`.
+         */
+        duration_in_traffic: Duration;
+        /**
+         * The total distance of this route, expressed in meters (`value`) and as `text`.
+         * The textual value uses the `unit` system specified with the unit parameter of the original request, or the origin's region.
+         */
+        distance: Distance;
+        /**
+         * If present, contains the total fare (that is, the total ticket costs) on this route.
+         * This property is only returned for transit requests and only for transit providers where fare information is available.
+         */
+        fare: TransitFare;
     }
 }
