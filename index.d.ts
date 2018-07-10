1d0d3a3be294401630d5792506fdf9e96371c3dd
Indri Muska <indrimuska@gmail.com>
2018-07-10 19:11:48 +0200

    Add `PlaceAutocomplete` API

--- index.d.ts
+++ index.d.ts
@@ -261,7 +261,13 @@ declare module '@google/maps' {
          * @see https://developers.google.com/places/web-service/search#TextSearchRequests
          */
         places: GoogleMapsClientEndpoint<PlacesRequest, PlaceSearchResponse>;
-        // placesAutoComplete: GoogleMapsClientEndpoint<Request, Response>;
+        /**
+         * The Place Autocomplete service is a web service that returns place predictions in response to an HTTP request.
+         * The request specifies a textual search string and optional geographic bounds.
+         * The service can be used to provide autocomplete functionality for text-based geographic searches,
+         * by returning places such as businesses, addresses and points of interest as a user types.
+         */
+        placesAutoComplete: GoogleMapsClientEndpoint<PlaceAutocompleteRequest, PlaceAutocompleteResponse>;
         // placesNearby: GoogleMapsClientEndpoint<Request, Response>;
         // placesPhoto: GoogleMapsClientEndpoint<Request, Response>;
         // placesQueryAutoComplete: GoogleMapsClientEndpoint<Request, Response>;
@@ -2285,5 +2291,200 @@ declare module '@google/maps' {
          * Only one type may be specified (if more than one type is provided, all types following the first entry are ignored).
          */
         type?: AddressType;
+    }
+
+    /**
+     * The Place Autocomplete service can match on full words as well as substrings.
+     * Applications can therefore send queries as the user types, to provide on-the-fly place predictions.
+     * 
+     * The returned predictions are designed to be presented to the user to aid them in selecting the desired place.
+     * You can send a Place Details request for more information about any of the places which are returned.
+     */
+    export interface PlaceAutocompleteRequest {
+        /**
+         * The text string on which to search. The Place Autocomplete service will return candidate matches
+         * based on this string and order results based on their perceived relevance.
+         */
+        input: string;
+        /**
+         * A random string which identifies an autocomplete
+         * [session](https://developers.google.com/places/web-service/autocomplete#session_tokens) for billing purposes.
+         * If this parameter is omitted from an autocomplete request, the request is billed independently
+         */
+        sessiontoken: string;
+        /**
+         * The position, in the input term, of the last character that the service uses to match predictions.
+         * For example, if the input is 'Google' and the `offset` is 3, the service will match on 'Goo'.
+         * The string determined by the `offset` is matched against the first word in the input term only.
+         * For example, if the input term is 'Google abc' and the offset is 3, the service will attempt to match against 'Goo abc'.
+         * If no `offset` is supplied, the service will use the whole term.
+         * The `offset` should generally be set to the position of the text caret.
+         */
+        offset?: number;
+        /** The point around which you wish to retrieve place information */
+        location?: LatLng;
+        /** The distance (in meters) within which to return place results. Note that setting a radius biases results to the indicated area, but may not fully restrict results to the specified area */
+        radius?: number;
+        /**
+         * The language code, indicating in which language the results should be returned, if possible.
+         * Searches are also biased to the selected language; results in the selected language may be given a higher ranking.
+         * See the list of supported languages and their codes.
+         * Note that we often update supported languages so this list may not be exhaustive.
+         * If language is not supplied, the Place Autocomplete service will attempt to use the native language
+         * of the domain from which the request is sent.
+         */
+        language?: string;
+        /** The types of place results to return */
+        types?: PlaceType;
+        /**
+         * A grouping of places to which you would like to restrict your results.
+         * Currently, you can use `components` to filter by up to 5 countries.
+         * Countries must be passed as a two character, ISO 3166-1 Alpha-2 compatible country code.
+         * For example: `components=country:fr` would restrict your results to places within France.
+         * Multiple countries must be passed as multiple `country:XX` filters, with the pipe character (`|`) as a separator.
+         * For example: `components=country:us|country:pr|country:vi|country:gu|country:mp` would restrict your results
+         * to places within the United States and its unincorporated organized territories.
+         */
+        components?: string[];
+        /**
+         * Returns only those places that are strictly within the region defined by `location` and `radius`.
+         * This is a restriction, rather than a bias, meaning that results outside this region
+         * will not be returned even if they match the user input.
+         */
+        strictbounds?: boolean;
+    }
+
+    /**
+     * You may restrict results from a Place Autocomplete request to be of a certain type by passing a types parameter.
+     * The parameter specifies a type or a type collection, as listed in the supported types below.
+     * If nothing is specified, all types are returned. In general only a single type is allowed.
+     * The exception is that you can safely mix the geocode and establishment types,
+     * but note that this will have the same effect as specifying no types.
+     */
+    export enum PlaceType {
+        /**
+         * instructs the Place Autocomplete service to return only geocoding results, rather than business results.
+         * Generally, you use this request to disambiguate results where the location specified may be indeterminate.
+         */
+        Geocode = 'geocode',
+        /**
+         * instructs the Place Autocomplete service to return only geocoding results with a precise address.
+         * Generally, you use this request when you know the user will be looking for a fully specified address.
+         */
+        Address = 'address',
+        /** instructs the Place Autocomplete service to return only business results */
+        Establishment = 'establishment',
+        /**
+         * the `(regions)` type collection instructs the Places service to return any result matching the following types:
+         *  - `locality`
+         *  - `sublocality`
+         *  - `postal_code`
+         *  - `country`
+         *  - `administrative_area_level_1`
+         *  - `administrative_area_level_2`
+         */
+        Regions = '(regions)',
+        /** the (cities) type collection instructs the Places service to return results that match `locality` or `administrative_area_level_3` */
+        Cities = '(cities)',
+    }
+
+    export interface PlaceAutocompleteResponse {
+        /** contains metadata on the request */
+        status: PlaceAutocompleteResponseStatus;
+        /**
+         * When the Places service returns a status code other than `OK`, there may be an additional `error_message` field
+         * within the response object. This field contains more detailed information about the reasons behind the given status code.
+         */
+        error_message: string;
+        /**
+         * contains an array of places, with information about the place.
+         * See [Place Autocomplete Results](https://developers.google.com/places/web-service/autocomplete#place_autocomplete_results)
+         * for information about these results. The Places API returns up to 5 results.
+         */
+        predictions: PlaceAutocompleteResult[];
+    }
+
+    /**
+     * The `status` field within the Place Autocomplete response object contains the status of the request,
+     * and may contain debugging information to help you track down why the Place Autocomplete request failed.
+     */
+    export enum PlaceAutocompleteResponseStatus {
+        /** indicates that no errors occurred and at least one result was returned */
+        OK = 'OK',
+        /** indicates that the search was successful but returned no results. This may occur if the search was passed a bounds in a remote location */
+        ZERO_RESULTS = 'ZERO_RESULTS',
+        /** indicates that you are over your quota */
+        OVER_QUERY_LIMIT = 'OVER_QUERY_LIMIT',
+        /** indicates that your request was denied, generally because of lack of an invalid key parameter */
+        REQUEST_DENIED = 'REQUEST_DENIED',
+        /** generally indicates that the input parameter is missing */
+        INVALID_REQUEST = 'INVALID_REQUEST',
+        /** indicates a server-side error; trying again may be successful */
+        UNKNOWN_ERROR = 'UNKNOWN_ERROR',
+    }
+
+    /**
+     * When the Places service returns JSON results from a search, it places them within a `predictions` array.
+     * Even if the service returns no results (such as if the `location` is remote) it still returns an empty `predictions` array.
+     * XML responses consist of zero or more `<prediction>` elements.
+     * 
+     * **Note:** The Place Autocomplete response does not include the `scope` or `alt_ids` fields that you may see
+     * in search results or place details. This is because Autocomplete returns only Google-scoped place IDs.
+     * It does not return app-scoped place IDs that have not yet been accepted into the Google Places database.
+     */
+    export interface PlaceAutocompleteResult {
+        /**
+         * contains the human-readable name for the returned result.
+         * For `establishment` results, this is usually the business name.
+         */
+        description: string;
+        /**
+         * is a textual identifier that uniquely identifies a place.
+         * To retrieve information about the place, pass this identifier in the `placeId` field of a Places API request.
+         */
+        place_id: string;
+        /**
+         * contains an array of terms identifying each section of the returned description
+         * (a section of the description is generally terminated with a comma).
+         */
+        terms: PredictionTerm[];
+        /**
+         * contains an array of types that apply to this place.
+         * For example: `[ "political", "locality" ]` or `[ "establishment", "geocode" ]`.
+         */
+        types: AddressType[];
+        /**
+         * contains an array with `offset` value and `length`. These describe the location of
+         * the entered term in the prediction result text, so that the term can be highlighted if desired.
+         */
+        matched_substrings: PredictionSubstring[];
+        /** contains details on the prediction */
+        structured_formatting: StructuredFormatting;
+    }
+
+    export interface PredictionTerm {
+        /** containing the text of the term */
+        value: string;
+        /** start position of this term in the description, measured in Unicode characters */
+        offset: number;
+    }
+
+    export interface PredictionSubstring {
+        /** location of the entered term */
+        offset: number;
+        /** length of the entered term */
+        length: number;
+    }
+    
+    export interface StructuredFormatting {
+        /** contains the main text of a prediction, usually the name of the place */
+        main_text: string;
+        /**
+         * contains an array with `offset` value and `length`. These describe the location of
+         * the entered term in the prediction result text, so that the term can be highlighted if desired.
+         */
+        main_text_matched_substrings: PredictionSubstring[];
+        /** contains the secondary text of a prediction, usually the location of the place */
+        secondary_text: string;
     }
 }
