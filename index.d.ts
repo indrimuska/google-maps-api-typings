b2763aaf8420ed49eff31a4623d49248a1bb88bd
Indri Muska <indrimuska@gmail.com>
2018-07-11 11:37:29 +0200

    Add `SnapToRoads` API

--- index.d.ts
+++ index.d.ts
@@ -327,9 +327,13 @@ declare module '@google/maps' {
          */
         snappedSpeedLimits: GoogleMapsClientEndpoint<SnappedSpeedLimitsRequest, SpeedLimitsResponse>;
         /**
+         * The Roads API takes up to 100 GPS points collected along a route, and returns a similar set of data,
+         * with the points snapped to the most likely roads the vehicle was traveling along.
+         * Optionally, you can request that the points be interpolated, resulting in a path that smoothly follows the geometry of the road.
+         * 
          * @see https://developers.google.com/maps/documentation/roads/snap
          */
-        // snapToRoads: GoogleMapsClientEndpoint<Request, Response>;
+        snapToRoads: GoogleMapsClientEndpoint<SnapToRoadsRequest, SnapToRoadsResponse>;
         /**
          * @see https://developers.google.com/maps/documentation/roads/speed-limits
          */
@@ -3028,5 +3032,34 @@ declare module '@google/maps' {
          * The `placeId` can be passed in a speed limits request to determine the speed limit along that road segment.
          */
         placeId: string;
+    }
+
+    export interface SnapToRoadsRequest {
+        /**
+         * The path to be snapped. The `path` parameter accepts a list of latitude/longitude pairs.
+         * Latitude and longitude values should be separated by commas. Coordinates should be separated by the pipe character: `"|"`.
+         * For example: `path=60.170880,24.942795|60.170879,24.942796|60.170877,24.942796`.
+         * 
+         * **Note:** The snapping algorithm works best for points that are not too far apart.
+         * If you observe odd snapping behavior, try creating paths that have points closer together.
+         * To ensure the best snap-to-road quality, you should aim to provide paths on which consecutive pairs
+         *  of points are within 300m of each other. This will also help in handling any isolated, long jumps between
+         * consecutive points caused by GPS signal loss, or noise.
+         */
+        path: LatLng[];
+        /**
+         * Whether to interpolate a path to include all points forming the full road-geometry.
+         * When true, additional interpolated points will also be returned, resulting in a path that smoothly follows
+         * the geometry of the road, even around corners and through tunnels.
+         * Interpolated paths will most likely contain more points than the original path.
+         * 
+         * @default false
+         */
+        interpolate?: boolean;
+    }
+
+    export interface SnapToRoadsResponse {
+        /** An array of snapped points */
+        snappedPoints: SnappedPoint[];
     }
 }
