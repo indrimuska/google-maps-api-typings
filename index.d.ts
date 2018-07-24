cc1a6de775d83136ca734ca594eba17c658a65e8
Indri Muska <indrimuska@gmail.com>
2018-07-24 18:16:52 +0200

    Fix Places API (findPlace)

--- index.d.ts
+++ index.d.ts
@@ -1521,7 +1521,7 @@ declare module '@google/maps' {
         /** The text input specifying which place to search for (for example, a name, address, or phone number). */
         input: string;
         /** The type of input. This can be one of either `textquery` or `phonenumber`. */
-        inputtype: string;
+        inputtype: 'textquery' | 'phonenumber';
         /**
          * The language code, indicating in which language the results should be returned, if possible.
          * Searches are also biased to the selected language; results in the selected language may be given a higher ranking
@@ -1532,7 +1532,7 @@ declare module '@google/maps' {
          * 
          * **Note:** If you omit the fields parameter from a Find Place request, only the place_id for the result will be returned.
          */
-        fields?: (keyof PlaceSearchResponse)[];
+        fields?: (keyof PlaceSearchResult)[];
         /**
          * Prefer results in a specified area, by specifying either a radius plus lat/lng, or two lat/lng pairs representing
          * the points of a rectangle. If this parameter is not specified, the API uses IP address biasing by default.
