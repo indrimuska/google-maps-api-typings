47f9afa8cb37afa4f5ed18d467e7cd7bc8138c3d
Simon Kostede <simon.kostede@de.neusoft.com>
2019-01-29 16:54:05 +0100

    fix of type definition
     - typo in line 3095
    
     - indrimuska/google-maps-api-typings#7

--- index.d.ts
+++ index.d.ts
@@ -3092,7 +3092,7 @@ declare module '@google/maps' {
          * contains an `offset` value and a `length`.
          * These describe the location of the entered term in the prediction result text, so that the term can be highlighted if desired.
          */
-        matched_substring: PredictionSubstring[];
+        matched_substrings: PredictionSubstring[];
     }
 
     /** A Radar Search request must include at least one of `keyword`, `name`, or `type`. */
